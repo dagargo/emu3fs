e37da741dcc6e10d34af50f5a2b4441e4e1c7784
dagargo <dagargo@gmail.com>
2018-05-19 18:27:42 +0200

    Set file fsync to generic_file_fsync

--- file.c
+++ file.c
@@ -25,7 +25,8 @@ const struct file_operations emu3_file_operations_file = {
 	.read_iter = generic_file_read_iter,
 	.write_iter = generic_file_write_iter,
 	.mmap = generic_file_mmap,
-	.splice_read = generic_file_splice_read
+	.splice_read = generic_file_splice_read,
+	.fsync = generic_file_fsync
 };
 
 static int
