2c8bd376e72f8b4663e010eeadf158298eed7f36
dagargo <dagargo@gmail.com>
2021-08-21 21:25:09 +0200

    Improve tests and their output

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+[ -z "$EMU3_TEST_DEBUG" ] && EMU3_TEST_DEBUG=0
+
 if [ "$EMU3_MOUNTPOINT" == "" ]; then
   echo "The variable EMU3_MOUNTPOINT must be set"
   exit -1
@@ -8,6 +10,7 @@ fi
 function test() {
   [ $? -eq 0 ] && ok=$((ok+1))
   total=$((total+1))
+  [ $EMU3_TEST_DEBUG -eq 1 ] && [ -n "$1" ] && ls -lai $EMU3_MOUNTPOINT/$1
   echo "Results: $ok/$total"
 }
 
@@ -33,23 +36,24 @@ rmdir $EMU3_MOUNTPOINT/foo
 test
 
 mkdir $EMU3_MOUNTPOINT/foo
+test
 
 echo "1234" > $EMU3_MOUNTPOINT/foo/t1
 echo "5678" >> $EMU3_MOUNTPOINT/foo/t1
 [ "12345678" != "$(< $EMU3_MOUNTPOINT/foo/t1)" ]
-test
+test foo/t1
 
 cp $EMU3_MOUNTPOINT/foo/t1 $EMU3_MOUNTPOINT/foo/t3
-test
+test foo/t3
 
 mv $EMU3_MOUNTPOINT/foo/t3 $EMU3_MOUNTPOINT/foo/t2
-test
+test foo/t2
 
 [ "$(< $EMU3_MOUNTPOINT/foo/t1)" == "$(< $EMU3_MOUNTPOINT/foo/t2)" ]
 test
 
 > $EMU3_MOUNTPOINT/foo/t1
-test
+test foo/t2
 
 [ 0 -eq $(wc -c $EMU3_MOUNTPOINT/foo/t1 | awk '{print $1}') ]
 test
@@ -57,10 +61,10 @@ test
 head -c 32M </dev/urandom > t3
 
 cp t3 $EMU3_MOUNTPOINT/foo
-test
+test foo/t2
 
 cp t3 $EMU3_MOUNTPOINT/foo/t4
-test
+test foo/t2
 
 echo "Remounting..."
 sudo umount $EMU3_MOUNTPOINT
@@ -79,21 +83,27 @@ test
 rm t3 t3.bak
 
 rm $EMU3_MOUNTPOINT/foo/t*
-test
+test foo
 
 rmdir $EMU3_MOUNTPOINT/foo
-test
+test .
 
 # Testing dir expansion
 
 mkdir $EMU3_MOUNTPOINT/expansion
+test expansion
 
-for i in $(seq 1 16); do touch $EMU3_MOUNTPOINT/expansion/f-$i; done
+for i in $(seq 1 16); do
+        name=f-${i}
+        touch $EMU3_MOUNTPOINT/expansion/$name
+        test expansion/$name
+done
+
 touch $EMU3_MOUNTPOINT/expansion/f-17
-test
+test expansion
 
 rm -rf $EMU3_MOUNTPOINT/expansion
-test
+test .
 
 echo "Cleaning up..."
 sudo umount $EMU3_MOUNTPOINT
