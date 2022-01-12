c4f20fb368b521b960bfdb4bdac0f1865e2227b4
Ray Bellis <ray@bellis.me.uk>
2022-01-12 01:13:55 +0000

    remove 64 bit division for RPi

--- super.c
+++ super.c
@@ -73,11 +73,11 @@ void emu3_set_fattrs(struct emu3_sb_info *info,
 		fattrs->blocks = cpu_to_le16(1);
 		fattrs->bytes = cpu_to_le16(0);
 	} else {
-		fattrs->clusters = size / info->bytes_per_cluster;
-		rem = size % info->bytes_per_cluster;
+		fattrs->clusters = size >> info->cluster_size_shift;
+		rem = size - (fattrs->clusters << info->cluster_size_shift);
 		if (rem)
 			fattrs->clusters++;
-		fattrs->blocks = rem / EMU3_BSIZE;
+		fattrs->blocks = rem >> EMU3_BSIZE_BITS;
 		rem = rem % EMU3_BSIZE;
 		if (rem)
 			fattrs->blocks++;
@@ -513,8 +513,8 @@ static int emu3_fill_super(struct super_block *sb, void *data,
 	info->start_cluster_list_block = le32_to_cpu(parameters[6]);
 	info->cluster_list_blocks = le32_to_cpu(parameters[7]);
 	info->start_data_block = le32_to_cpu(parameters[8]);
-	info->blocks_per_cluster = (0x10000 << (e3sb[0x28] - 1)) / EMU3_BSIZE;
-	info->bytes_per_cluster = info->blocks_per_cluster * EMU3_BSIZE;
+	info->cluster_size_shift = 15 + e3sb[0x28];	//32kB minimum
+	info->blocks_per_cluster = 1 << (info->cluster_size_shift - EMU3_BSIZE_BITS);
 	//In Formula 4000 only, the total amount of blocks and clusters would allow to have a disk bigger than the ISO image itself.
 	//Thus, the reported amount of blocks, size and free space is not right.
 	//However, if the iso image is resized to accommodate all the blocks, the format is valid and stat and df output the right values.
